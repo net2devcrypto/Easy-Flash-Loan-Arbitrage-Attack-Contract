e19cdc41f2ae1f2241c0a987342956b6f120f974
Net2Dev <info@a3b.io>
2024-03-29 17:25:58 -0700

    Update N2D_Sample_AAVE_Flash_Loan_SWAP_Attack_Contract.sol

--- N2D_Sample_AAVE_Flash_Loan_SWAP_Attack_Contract.sol
+++ N2D_Sample_AAVE_Flash_Loan_SWAP_Attack_Contract.sol
@@ -11,6 +11,9 @@ Twitter: https://twitter.com/cookbook_dev
 Discord: https://discord.gg/cookbookdev
 
 Find this contract on Cookbook: https://www.cookbook.dev/contracts/FlashloanAttacker?utm=code
+
+PLEASE DO NOT DEPLOY ON A MAINNET, ONLY ON A TESTNET
+I'M NOT RESPONSIBLE FOR ANY USE, LOSS OF FUNDS OR ANY OTHER ISSUES.
 */
 
 // SPDX-License-Identifier: BUSL-1.1
